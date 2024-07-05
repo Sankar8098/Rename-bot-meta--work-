12bd71c1e4deee11c5a9d51ef8d0222b679b702b
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-05 10:40:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ id_pattern = re.compile(r'^.\d+$')
 
 API_ID = int(os.environ.get("API_ID", "23990433"))
 API_HASH = os.environ.get("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7090403697:AAGjSKZOl_kPksvKJNaKbiBl5XDI0tt5QpY")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5992501587:AAFyt9Fixl8cMymWvObeiBBZZVmO4hwkOgs")
 ADMIN = int(os.environ.get("ADMIN", '5821871362')) 
 FSUB_UPDATES = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
 FSUB_GROUP = os.environ.get("FSUB_GROUP", "Sunrises24BotSupport")
@@ -16,7 +16,7 @@ CAPTION = os.environ.get("CAPTION", "")
 #ALL FILES UPLOADED - CREDITS 🌟 - @Sunrises_24
 DOWNLOAD_LOCATION = "./DOWNLOADS"
 SUNRISES_PIC= "https://graph.org/file/bd91761f6e938e2e6d23a.jpg"  # Replace with your Telegraph link
-AUTH_USERS = int(os.environ.get("AUTH_USERS", '6469754522'))
+AUTH_USERS = int(os.environ.get("AUTH_USERS", '5821871362'))
 
 
 
